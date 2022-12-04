912548d9e8ef9f9e4a7f93aa5e992fb5ee44b5ec
skleinle <Simon.Kleinle1@HS-Augsburg.DE>
2022-12-04 19:58:22 +0100

    Add funken to arduino sketch

--- sketch_dec03a.ino
+++ sketch_dec03a.ino
@@ -1,56 +1,51 @@
-/*
-  Physical Pixel
+// Funken library
+// Download the Funken library here: https://github.com/astefas/Funken/tree/master/bin
+// Install with Sketch > Include Library > Add .ZIP Library
+#include <Funken.h>
 
-  An example of using the Arduino board to receive data from the computer. In
-  this case, the Arduino boards turns on an LED when it receives the character
-  'H', and turns off the LED when it receives the character 'L'.
-  The data can be sent from the Arduino Serial Monitor, or another program like
-  Processing (see code below), Flash (via a serial-net proxy), PD, or Max/MSP.
-  The circuit:
-  - LED connected from digital pin 13 to ground
-
-  created 2006
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe and Scott Fitzgerald
-  This example code is in the public domain.
-  http://www.arduino.cc/en/Tutorial/PhysicalPixel
-*/
 #include <Wire.h>
 #include "rgb_lcd.h"
 
+Funken fnk;
 rgb_lcd lcd;
+
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
+bool alarm=false;
+
 const int ledPin = 2; // the pin that the LED is attached to
-int incomingByte;      // a variable to read incoming serial data into
-bool h = false;
-String s = "";
 
 void setup() {
+  /*
   // initialize serial communication:
   Serial.begin(57600);
+  */
+  fnk.begin(57600, 0, 0); // higher baudrate for better performance
+  fnk.listenTo("TEST", test); // however you want to name your callback
+  
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-
   lcd.setRGB(colorR, colorG, colorB);
   lcd.begin(16, 2);
 }
 
 void loop() {
-  Serial.println(colorR);
-  lcd.print(s);
-  if (h) {
+  // needed to make FUNKEN work
+  fnk.hark();
+
+  
+  if (alarm) {
     digitalWrite(ledPin, HIGH);   // set the LED on
     delay(500);               // for 500ms
     digitalWrite(ledPin, LOW);   // set the LED off
     delay(500);
   }
+  
+  /*
   // see if there's incoming serial data:
   if (Serial.available() > 0) {
     // read the oldest byte in the serial buffer:
@@ -66,4 +61,21 @@ void loop() {
       digitalWrite(ledPin, LOW);
     }
   }
+  */
+}
+
+void test(char *c) {
+
+  // get first argument
+  char *token = fnk.getToken(c); // is needed for library to work properly, but can be ignored
+
+  String s = "" + atoi(fnk.getArgument(c));
+
+  lcd.print(s);
+
+  if(s=="h")
+  {
+    alarm=true;
+  }
+
 }
