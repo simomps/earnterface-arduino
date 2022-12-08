bbb90816ab9467bc4d03d6a80eb624752ceb5fa5
Simon Kleinle <simon.kleinle1@hs-augsburg.de>
2022-12-08 14:30:55 +0100

    x

--- earnterface-arduino.ino
+++ earnterface-arduino.ino
@@ -55,7 +55,7 @@ void loop() {
   if (Serial.available() > 0) {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
-    s = Serial.readString();
+    s = Serial.read();
     // if it's a capital H (ASCII 72), turn on the LED:
     if (incomingByte == 'H') {
       h = true;
