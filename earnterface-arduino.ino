c9e195883c52d4d5eb7446b931b3a06266e45cdc
Simon Kleinle <simon.kleinle1@hs-augsburg.de>
2023-01-17 15:16:15 +0100

    Add changes of error program

--- earnterface-arduino.ino
+++ earnterface-arduino.ino
@@ -6,7 +6,10 @@ Servo myservo;  // create servo object to control a servo
 Servo myservo1;
 // twelve servo objects can be created on most boards
 int pos = 0;    // variable to store the servo position
-int incomingByte = 0;   // for incoming serial data
+String incomingByte;   // for incoming serial data
+
+int trigger = 0;
+unsigned long lastMillis;
 
 void setup() {
   // initialize serial communication:
@@ -17,14 +20,12 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(lastMillis);
   // see if there's incoming serial data:
   if (Serial.available() > 0) {
-    lcd.print(incomingByte);
 
     incomingByte = Serial.readStringUntil('\n');
     // read the incoming byte:
-    incomingByte = Serial.read();
-
 
     if (incomingByte == "ACTION_1") {
       // DO SOME ACTION HERE
@@ -36,18 +37,19 @@ void loop() {
       // DO SOME ACTION HERE
     }
     if (trigger == 1) {
+
       //change cycle based on timer
-      if(lastMillis >= 30L * 1000 && lastMillis < 60L * 1000){ //half a minute for left turn
+      if(millis() - lastMillis >= 30L * 1000 && millis() - lastMillis < 60L * 1000){ //half a minute for left turn
        myservo.write(0);
        myservo1.write(0);
-      }else if(lastMillis >= 60L * 1000 && lastMillis < 90L * 1000){//half a minute for right turn
+      }else if(millis() - lastMillis >= 60L * 1000 && millis() - lastMillis < 90L * 1000){//half a minute for right turn
         myservo.write(180);
         myservo1.write(180); 
-      }else if(lastMillis >= 90L * 1000){ //after 1 min stop
+      }else if(millis() - lastMillis >= 90L * 1000){ //after 1 min stop
         myservo.write(90);
         myservo1.write(90);
       }
-      delay(1000);               // for 1000ms
+      //delay(1000);               // for 1000ms
       Serial.write("test");
     }
   }
