7380dc13a4ef5227ed878126fe0bbef5e08702dd
Simon Kleinle <simon.kleinle1@hs-augsburg.de>
2023-01-19 13:16:35 +0100

    Change timer to delay() because ARDUINO is SHIT

--- earnterface-arduino.ino
+++ earnterface-arduino.ino
@@ -8,15 +8,15 @@ Servo myservo1;
 String incomingByte;   // for incoming serial data
 
 int myState = 0;
-long myStartTime = 0;
 
 const long time1 = 2000; // 20 sec
-const long time2 = 4000; // 40 sec
+const long time2 = 2000; // 40 sec
+const long time3 = 2000; // 40 sec
 
 void setup() {
   // initialize serial communication:
   Serial.begin(57600);
-  
+
   myservo.attach(6);  // attaches the servo on pin 9 to the servo object
   myservo1.attach(9);
 }
@@ -31,35 +31,28 @@ void loop() {
     if (incomingByte == "ACTION_1") {
       // DO SOME ACTION HERE
       myState = 1;
-      //START TIMER
-      myStartTime = millis();
-      Serial.println("HEY");
     }
     if (incomingByte == "ACTION_2") {
       // DO SOME ACTION HERE
     }
     if (myState == 1) {
-      Serial.println("last: ");
-      long m = millis() - myStartTime;
-      Serial.write(" " + m);
-      //change cycle based on timer
-      if(millis() - myStartTime < time1){ //for left turn
-       myservo.write(0);
-       myservo1.write(0);
-       Serial.write(" :1");
-      }
-      if(millis() - myStartTime >= time1 && millis() - myStartTime < time2){ //for right turn
-        myservo.write(180);
-        myservo1.write(180); 
-        Serial.write(" :2");
-      }
-      if(millis() - myStartTime >= time2){ //after 1 min stop
-        myservo.write(90);
-        myservo1.write(90);
-        Serial.write(" :3");
-      }
-      //delay(1000);               // for 1000ms
       Serial.write("test");
+      //change cycle based on timer
+      myservo.write(0);
+      myservo1.write(0);
+      delay(time1);
+
+      myservo.write(90);
+      myservo1.write(90);
+      delay(time2);
+
+      myservo.write(180);
+      myservo1.write(180);
+      delay(time3);
+
+      myservo.write(90);
+      myservo1.write(90);
+
     }
   }
-} 
+}
